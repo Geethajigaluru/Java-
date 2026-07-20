b28d2aea641dcc9b2abc0bedb594d2443ecd5eb3
Geethajigaluru <geethajigalur06@gmail.com>
2026-07-20 18:48:32 +0530

    Added if-else program

---
+++ Java_Programs/If_else.java
@@ -0,0 +1,60 @@
+public class If_else{
+    public static void main(String[] args){
+        int num=-8;
+        if (num>=0){
+            System.out.println("Positive Number");
+        }
+        else{
+            System.out.println("Negative Number");
+        }
+        }
+    }
+
+public class If_else{
+    public static void main(String[] args){
+        int num=17;
+        if(num%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
+
+public class If_else{
+    public static void main(String[] args){
+        int marks=38;
+        if(marks>=40)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+}
+
+public class If_else{
+    public static void main(String[] args){
+        int age=19;
+        if (age>=18){
+            System.out.println("Eligible to Vote");
+        }
+        else{
+            System.out.println("Not Eligible to Vote");
+        }
+    }
+}
+
+public class If_else{
+    public static void main(String[] args){
+        int a=25;
+        int b=40;
+        if (a>b){
+            System.out.println("a is greater");
+        }
+        else{
+            System.out.println("b is greater");
+        }
+        }
+    }
+
+
