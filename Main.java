486a2c4080eddfbd43ff4cdcc0f898b71d52aafb
Geethajigaluru <geethajigalur06@gmail.com>
2026-08-03 20:11:15 +0530

    Added class_object file

---
+++ Main.java
@@ -0,0 +1,80 @@
+//Q1
+class Student{
+    String name;
+    int age;
+}
+public class Main{
+    public static void main(String[] args){
+        Student s1=new Student();
+        s1.name="Geetha";
+        s1.age=20;
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+
+    }
+}
+    
+//Q2
+class Car{
+    String brand;
+    String color;
+}
+public class Main{
+    public static void main(String[] args){
+        Car c1=new Car();
+        c1.brand="BMW";
+        c1.color="Black";
+        System.out.println(c1.brand);
+        System.out.println(c1.color);
+    }
+}
+    
+//Q3
+class Book{
+    String title;
+    int price;
+}
+public class Main{
+    public static void main(String[] args){
+        Book b1=new Book();
+        b1.title="Java Programming";
+        b1.price=599;
+        System.out.println(b1.title);
+        System.out.println(b1.price);
+    }
+}
+    
+//Q4
+class Mobile{
+    String brand;
+    int RAM;
+}
+public class Main{
+    public static void main(String[] args){
+        Mobile m1=new Mobile();
+        Mobile m2=new Mobile();
+        m1.brand="Samsung";
+        m1.RAM=8;
+        m2.brand="OnePlus";
+        m2.RAM=12;
+        System.out.println(m1.brand);
+        System.out.println(m1.RAM);
+        System.out.println(m2.brand);
+        System.out.println(m2.RAM);
+    }
+}
+    
+//Q5
+class Employee{
+    String name;
+    int Salary;
+}
+public class Main{
+    public static void main(String[] args){
+        Employee e1=new Employee();
+        e1.name="Geetha";
+        e1.Salary=100000;
+        System.out.println(e1.name);
+        System.out.println(e1.Salary);
+    }
+}
